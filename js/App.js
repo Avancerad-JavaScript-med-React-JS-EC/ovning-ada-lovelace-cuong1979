2fe5455d8885227e0be3591c118a77e433b1a650
cuong1979 <70330972+cuong1979@users.noreply.github.com>
2020-12-17 11:54:58 +0100

    klart

---
+++ js/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from './Image'
+import About from './About'
+
+function App(){
+    return(
+        <div>
+            <h1>Ada Lovelace</h1>
+            <Image />
+            <About />
+
+        </div>
+        
+        
+    )
+    
+}
+
+
+export default App
---
+++ js/Image.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Image(){
+
+    return(
+        <img className="imag" src= 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Ada_Lovelace_portrait.jpg'></img>
+    )
+}
+
+export default Image;
+
+
+
